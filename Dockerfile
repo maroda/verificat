6e42b6b42c6358e812cc4c30d418a1d20f39857b
Matt Davis <maroda@gmail.com>
2025-10-02 20:44:40 -0700

    back to alpine:latest, log endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.25-alpine3.22
+FROM alpine:latest
 LABEL app=verificat
 LABEL org.opencontainers.image.source=https://github.com/maroda/verificat
 WORKDIR /
--- obvy/honeycomb.go
+++ obvy/honeycomb.go
@@ -2,12 +2,15 @@ package verificat
 
 import (
 	"fmt"
+	"log/slog"
 
 	"github.com/honeycombio/otel-config-go/otelconfig"
 )
 
 func InitOTel() (func(), error) {
 	otelShutdown, err := otelconfig.ConfigureOpenTelemetry()
+	config := otelconfig.DefaultExporterEndpoint
+	slog.Info("Initializing otel", slog.String("endpoint", config))
 	if err != nil {
 		return nil, fmt.Errorf("failed to configure OpenTelemetry: %w", err)
 	}
